3d5e897f16f4b9ab9524a95ad619f8c60a062b74
Nour Hrs <nour.fwh@gmail.com>
2016-09-02 17:06:56 +0200

    add all parsers from exercises

--- ch2_0.hs
+++ ch2_0.hs
@@ -3,6 +3,9 @@ module Main where
 import Control.Monad
 import System.Environment
 import Text.ParserCombinators.Parsec hiding (spaces)
+import Numeric
+import Data.Ratio
+import Data.Complex
 
 data LispVal = Atom String
   | List [LispVal]
@@ -10,12 +13,30 @@ data LispVal = Atom String
   | Number Integer
   | String String
   | Bool Bool
+  | Character Char
+  | Float Float
+  | Rational Rational
+  | Complex (Complex Double)
   deriving (Show)
 
 main :: IO ()
 main = do
   (expr:_) <- getArgs
   putStrLn (readExpr expr)
+
+readExpr :: String -> String
+readExpr input = case parse parseExpr "lisp" input of
+  Left err  -> "No match: " ++ show err
+  Right val -> show val -- "Found value"
+
+parseExpr :: Parser LispVal
+parseExpr = try parseComplex -- readExpr "3+4i"
+  <|> try parseRational -- readExpr "3/4"
+  <|> try parseFloat -- readExpr "3.4"
+  <|> try parseNumber -- readExpr "3"
+  <|> try parseChar -- readExpr "#\\A"
+  <|> parseString -- readExpr "\"hello hows it going?\"" | readExpr "\"hello how\\\"s it going?\""
+  <|> parseAtom -- readExpr "test"
 
 symbol :: Parser Char
 symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
@@ -26,9 +47,19 @@ spaces = skipMany1 space
 parseString :: Parser LispVal
 parseString = do
   char '"'
-  x <- many (noneOf "\"")
+  x <- many (escapedChars <|> noneOf "\"")
   char '"'
   return $ String x
+
+parseChar :: Parser LispVal
+parseChar = do
+  try $ string "#\\"
+  x <- try (string "space" <|> string "newline")
+    <|> many (anyChar)
+  return $ Character $ case x of
+    "space" -> ' '
+    "newline" -> '\n'
+    _ -> head x
 
 parseAtom :: Parser LispVal
 parseAtom = do
@@ -41,14 +72,55 @@ parseAtom = do
     _    -> Atom atom
 
 parseNumber :: Parser LispVal
-parseNumber = liftM (Number . read) $ many1 digit
+parseNumber = parseDecimal <|> parseExtended
 
-parseExpr :: Parser LispVal
-parseExpr = parseAtom
-  <|> parseNumber
-  <|> parseString
+parseDecimal :: Parser LispVal
+parseDecimal = liftM (Number . read) $ many1 digit
 
-readExpr :: String -> String
-readExpr input = case parse parseExpr "lisp" input of
-  Left err  -> "No match: " ++ show err
-  Right val -> show val -- "Found value"
+parseExtended :: Parser LispVal
+parseExtended = do
+  char '#'
+  c <- oneOf "xod"
+  case c of
+    'x' -> liftM (Number . fst . head . readHex) $ many1 (letter <|> digit)
+    'o' -> liftM (Number . fst . head . readOct) $ many1 digit
+    'd' -> parseDecimal
+
+parseFloat :: Parser LispVal
+parseFloat = do
+  x <- many digit
+  y <- char '.'
+  z <- many digit
+  let (a, b) = head $ readFloat (x ++ [y] ++ z)
+  return (Float a)
+
+parseRational :: Parser LispVal
+parseRational = do
+  x <- many digit
+  char '/'
+  z <- many digit
+  return $ Rational ((read x) % (read z))
+
+-- "3+4i" gets parsed to: Complex (3.0 :+ 4.0)
+parseComplex :: Parser LispVal
+parseComplex = do
+  x <- (try parseFloat <|> parseDecimal)
+  char '+'
+  y <- (try parseFloat <|> parseDecimal)
+  char 'i'
+  return $ Complex (toDouble x :+ toDouble y)
+
+escapedChars :: Parser Char
+escapedChars = do
+  char '\\'
+  x <- oneOf "\\\"nrt"
+  return $ case x of
+    '\\' -> x
+    '\"' -> x
+    'n'  -> '\n'
+    'r'  -> '\r'
+    't'  -> '\t'
+
+toDouble :: LispVal -> Double
+toDouble(Float f) = realToFrac f
+toDouble(Number n) = fromIntegral n
